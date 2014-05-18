d78e09f8bc623b1dadcbea9ece1b9c307c1a2475
loveiset <loveiset@qq.com>
2014-05-18 22:46:16 +0800

    还差碰撞相关和屏幕尺寸适配

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed = -1;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        rigidbody2D.velocity = new Vector2(speed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
